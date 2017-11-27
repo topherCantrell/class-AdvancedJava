cd29e2d5713c0d279fc8fb16a995be437741087d
tophe <tophe@DESKTOP-EC8N5HK>
2017-11-27 17:46:17 -0600

    Moved

--- Books/src/commondb/PublisherDAO.java
+++
@@ -1,9 +0,0 @@
-package commondb;
-
-import books.Publisher;
-
-public interface PublisherDAO {    
-    public Publisher getAll() throws DAOException;
-    public Publisher create(String name, String address) throws DAOException;
-    // ...
-}
